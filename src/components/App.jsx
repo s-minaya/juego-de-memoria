9668f63fa105c6719cf704d68e233f48cce0120e
Eilith404 <minaya.sofia@gmail.com>
2025-11-30 21:41:18 +0100

    Added some styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,21 @@ import "../styles/App.scss";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <header className="header">
-        <h1 className="title">Plantilla React</h1>
+        <h1 className="title">Juego de memoria</h1>
       </header>
-      <main className="main">Holis Adalabers!!!</main>
+      <main className="main">
+
+        <ul className="board">
+          <li className="card"><img className="card-img"src="/src/images/nigiris.png" alt="icono-de-nigiri" /></li>
+          <li className="card"><img className="card-img"src="/src/images/onigiri.png" alt="icono-de-onigiri" /></li>
+          <li className="card"><img className="card-img"src="/src/images/ramen.png" alt="icono-de-ramen" /></li>
+          <li className="card"><img className="card-img"src="/src/images/sushi.png" alt="icono-de-sushi" /></li>
+          <li className="card"><img className="card-img"src="/src/images/te-matcha.png" alt="icono-de-te-matcha" /></li>
+          <li className="card"><img className="card-img"src="/src/images/tempura.png" alt="icono-de-tempura" /></li>
+        </ul>
+      </main>
     </div>
   );
 }
